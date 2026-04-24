f0bc7908114f6af9f4f664f3ad26ad86d2bc0a73
wuhanstudio <wuhanstudios@gmail.com>
2026-04-24 14:42:32 +0100

    [fix] Upgrade from Embedded-HAL v0.2 to v1.0

--- src/main.rs
+++ src/main.rs
@@ -3,85 +3,97 @@
 
 use panic_halt as _;
 
+use cortex_m::asm::delay;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{
     pac,
     prelude::*,
-    spi::{Mode, Phase, Polarity, Spi},
+    spi::{Mode, Phase, Polarity},
     usb::{Peripheral, UsbBus},
 };
+use stm32f1xx_hal::rcc;
+
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 mod serprog;
-use serprog::{SerprogCommand, SerprogResponse, SerprogState};
+use serprog::SerprogState;
+
+use rtt_target::{rtt_init_print, rprintln};
 
 // SPI Mode 0: CPOL=0, CPHA=0
-const SPI_MODE: Mode = Mode {
-    polarity: Polarity::IdleLow,
-    phase: Phase::CaptureOnFirstTransition,
+// const SPI_MODE: Mode = Mode {
+//     polarity: Polarity::IdleLow,
+//     phase: Phase::CaptureOnFirstTransition,
+// };
+
+pub const MODE: Mode = Mode {
+    phase: Phase::CaptureOnSecondTransition,
+    polarity: Polarity::IdleHigh,
 };
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+
     // Get device peripherals
-    let dp = pac::Peripherals::take().unwrap();
-    
-    // Configure clocks
+    let Some(dp) = pac::Peripherals::take() else {
+        rprintln!("Failed to take peripheral ownership");
+        loop {}
+    };
+
     let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
-    
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.MHz())
-        .sysclk(72.MHz())
-        .pclk1(36.MHz())
-        .freeze(&mut flash.acr);
-    
-    assert!(clocks.usbclk_valid());
+    let mut rcc = dp.RCC.freeze(
+        rcc::Config::hse(8.MHz()).sysclk(48.MHz()).pclk1(24.MHz()),
+        &mut flash.acr,
+    );
     
     // Configure GPIO
-    let mut gpioa = dp.GPIOA.split();
-    let mut gpiob = dp.GPIOB.split();
+    let mut gpioa = dp.GPIOA.split(&mut rcc);
+    let mut gpiob = dp.GPIOB.split(&mut rcc);
     
     // SPI1 pins: SCK=PA5, MISO=PA6, MOSI=PA7
     let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
     let miso = gpioa.pa6;
     let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
-    
+
     // CS pin: PB0 (manually controlled)
     let mut cs = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
     cs.set_high(); // CS inactive (high)
-    
-    // Configure SPI at 9 MHz (PCLK2/8 = 72MHz/8)
-    let mut spi = Spi::spi1(
-        dp.SPI1,
-        (sck, miso, mosi),
-        SPI_MODE,
-        9.MHz(),
-        clocks,
-    );
-    
-    // Configure USB
-    let mut gpioa_usb = gpioa.crl;
-    let usb_dm = gpioa.pa11;
-    let usb_dp = gpioa.pa12.into_floating_input(&mut gpioa.crh);
-    
+
+    // let sck = gpioa.pa5;
+    // let miso = gpioa.pa6;
+    // let mosi = gpioa.pa7;
+    // let cs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
+
+    let mut spi = dp
+        .SPI1
+        //.remap(&mut afio.mapr) // if you want to use PB3, PB4, PB5
+        .spi((Some(sck), Some(miso), Some(mosi)), MODE, 9.MHz(), &mut rcc);
+
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();
+    delay(rcc.clocks.sysclk().raw() / 100);
+
     let usb = Peripheral {
         usb: dp.USB,
-        pin_dm: usb_dm,
-        pin_dp: usb_dp,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
     };
-    
     let usb_bus = UsbBus::new(usb);
-    
     let mut serial = SerialPort::new(&usb_bus);
-    
+
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("STM32")
-        .product("serprog")
-        .serial_number("SERPROG01")
         .device_class(USB_CLASS_CDC)
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake Company")
+            .product("Serial port")
+            .serial_number("TEST")])
+        .unwrap()
         .build();
     
     let mut serprog_state = SerprogState::new();
--- src/serprog.rs
+++ src/serprog.rs
@@ -1,30 +1,30 @@
-use embedded_hal::blocking::spi::Transfer;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::spi::SpiBus;
+use embedded_hal::digital::OutputPin;
 
 // Serprog protocol commands
-#[derive(Debug, Clone, Copy)]
-pub enum SerprogCommand {
-    Nop = 0x00,
-    QIface = 0x01,
-    QCmdmap = 0x02,
-    QPgmname = 0x03,
-    QSerbuf = 0x04,
-    QBustype = 0x05,
-    QChipsize = 0x06,
-    QOpbuf = 0x07,
-    QWrnmaxlen = 0x08,
-    RByte = 0x09,
-    RNbytes = 0x0A,
-    OInit = 0x0B,
-    ODelay = 0x0C,
-    OExec = 0x0D,
-    SyncNop = 0x0E,
-    QRdnmaxlen = 0x0F,
-    SSpiOp = 0x10,
-    SBustype = 0x11,
-    OSpiop = 0x12,
-    SPin = 0x13,
-}
+// #[derive(Debug, Clone, Copy)]
+// pub enum SerprogCommand {
+//     Nop = 0x00,
+//     QIface = 0x01,
+//     QCmdmap = 0x02,
+//     QPgmname = 0x03,
+//     QSerbuf = 0x04,
+//     QBustype = 0x05,
+//     QChipsize = 0x06,
+//     QOpbuf = 0x07,
+//     QWrnmaxlen = 0x08,
+//     RByte = 0x09,
+//     RNbytes = 0x0A,
+//     OInit = 0x0B,
+//     ODelay = 0x0C,
+//     OExec = 0x0D,
+//     SyncNop = 0x0E,
+//     QRdnmaxlen = 0x0F,
+//     SSpiOp = 0x10,
+//     SBustype = 0x11,
+//     OSpiop = 0x12,
+//     SPin = 0x13,
+// }
 
 #[derive(Debug)]
 pub enum SerprogResponse {
@@ -38,72 +38,72 @@ pub enum SerprogResponse {
     OperationBufferSize(u16),
     WriteNMaxLen(u16),
     ReadNMaxLen(u16),
-    ReadData(usize), // Index into data buffer
+    ReadData(usize),
 }
 
 impl SerprogResponse {
     pub fn to_bytes<'a>(&self, buf: &'a mut [u8]) -> &'a [u8] {
         match self {
             SerprogResponse::Ack => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 &buf[..1]
             }
             SerprogResponse::Nak => {
-                buf[0] = 0x15; // NAK
+                buf[0] = 0x15;
                 &buf[..1]
             }
             SerprogResponse::InterfaceVersion => {
-                buf[0] = 0x06; // ACK
-                buf[1] = 0x01; // Version 1
+                buf[0] = 0x06;
+                buf[1] = 0x01;
                 buf[2] = 0x00;
                 &buf[..3]
             }
             SerprogResponse::CommandMap(map) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1..33].copy_from_slice(map);
                 &buf[..33]
             }
             SerprogResponse::ProgrammerName(name) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 let name_bytes = name.as_bytes();
                 let len = name_bytes.len().min(15);
                 buf[1..1 + len].copy_from_slice(&name_bytes[..len]);
-                buf[1 + len] = 0; // Null terminator
+                buf[1 + len] = 0;
                 &buf[..2 + len]
             }
             SerprogResponse::SerialBufferSize(size) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1] = (*size & 0xFF) as u8;
                 buf[2] = (*size >> 8) as u8;
                 &buf[..3]
             }
             SerprogResponse::BusTypes(bus) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1] = *bus;
                 &buf[..2]
             }
             SerprogResponse::OperationBufferSize(size) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1] = (*size & 0xFF) as u8;
                 buf[2] = (*size >> 8) as u8;
                 &buf[..3]
             }
             SerprogResponse::WriteNMaxLen(len) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1] = (*len & 0xFF) as u8;
                 buf[2] = (*len >> 8) as u8;
                 buf[3] = 0x00;
                 &buf[..4]
             }
             SerprogResponse::ReadNMaxLen(len) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 buf[1] = (*len & 0xFF) as u8;
                 buf[2] = (*len >> 8) as u8;
                 buf[3] = 0x00;
                 &buf[..4]
             }
             SerprogResponse::ReadData(len) => {
-                buf[0] = 0x06; // ACK
+                buf[0] = 0x06;
                 &buf[..1 + len]
             }
         }
@@ -112,7 +112,7 @@ impl SerprogResponse {
 
 enum ParseState {
     WaitingForCommand,
-    SSpiOp { bytes_remaining: usize, write_len: usize, read_len: usize },
+    SSpiOp { bytes_remaining: usize },
     RNbytes { bytes_remaining: usize },
     SBustype,
     ODelay { bytes_remaining: u8 },
@@ -129,42 +129,41 @@ impl SerprogState {
     pub fn new() -> Self {
         Self {
             state: ParseState::WaitingForCommand,
-            buffer: [0u8; 512],
+            buffer: [0; 512],
             buffer_pos: 0,
             spi_initialized: false,
         }
     }
-    
+
     fn get_command_map() -> [u8; 32] {
         let mut map = [0u8; 32];
-        // Set bits for supported commands
-        map[0] = 0xFF; // 0x00-0x07 (NOP through QOpbuf)
-        map[1] = 0xFF; // 0x08-0x0F (QWrnmaxlen through QRdnmaxlen)
-        map[2] = 0x1F; // 0x10-0x13 (SSpiOp, SBustype, OSpiop, SPin)
+        map[0] = 0xFF;
+        map[1] = 0xFF;
+        map[2] = 0x1F;
         map
     }
-    
-    pub fn process_byte<SPI, CS, E>(
+
+    pub fn process_byte<SPI, CS>(
         &mut self,
         byte: u8,
         spi: &mut SPI,
         cs: &mut CS,
     ) -> Option<SerprogResponse>
     where
-        SPI: Transfer<u8, Error = E>,
+        SPI: SpiBus<u8>,
         CS: OutputPin,
     {
         match &mut self.state {
             ParseState::WaitingForCommand => {
                 self.handle_command(byte, spi, cs)
             }
-            ParseState::SSpiOp { bytes_remaining, write_len, read_len } => {
+            ParseState::SSpiOp { bytes_remaining } => {
                 self.buffer[self.buffer_pos] = byte;
                 self.buffer_pos += 1;
                 *bytes_remaining -= 1;
-                
+
                 if *bytes_remaining == 0 {
-                    let result = self.execute_spi_op(*write_len, *read_len, spi, cs);
+                    let result = self.execute_spi_op(spi, cs);
                     self.state = ParseState::WaitingForCommand;
                     self.buffer_pos = 0;
                     Some(result)
@@ -176,9 +175,8 @@ impl SerprogState {
                 self.buffer[self.buffer_pos] = byte;
                 self.buffer_pos += 1;
                 *bytes_remaining -= 1;
-                
+
                 if *bytes_remaining == 0 {
-                    // For now, just return NAK as we don't support parallel reading
                     self.state = ParseState::WaitingForCommand;
                     self.buffer_pos = 0;
                     Some(SerprogResponse::Nak)
@@ -188,16 +186,13 @@ impl SerprogState {
             }
             ParseState::SBustype => {
                 self.state = ParseState::WaitingForCommand;
-                // Just acknowledge - we only support SPI
                 Some(SerprogResponse::Ack)
             }
             ParseState::ODelay { bytes_remaining } => {
                 *bytes_remaining -= 1;
-                
                 if *bytes_remaining == 0 {
                     self.state = ParseState::WaitingForCommand;
-                    // Simple delay implementation
-                    cortex_m::asm::delay(72000); // ~1ms at 72MHz
+                    cortex_m::asm::delay(72000);
                     Some(SerprogResponse::Ack)
                 } else {
                     None
@@ -205,131 +200,108 @@ impl SerprogState {
             }
         }
     }
-    
-    fn handle_command<SPI, CS, E>(
+
+    fn handle_command<SPI, CS>(
         &mut self,
         cmd: u8,
         _spi: &mut SPI,
         _cs: &mut CS,
     ) -> Option<SerprogResponse>
     where
-        SPI: Transfer<u8, Error = E>,
+        SPI: SpiBus<u8>,
         CS: OutputPin,
     {
         match cmd {
-            0x00 => Some(SerprogResponse::Ack), // NOP
-            
-            0x01 => Some(SerprogResponse::InterfaceVersion), // Q_IFACE
-            
-            0x02 => Some(SerprogResponse::CommandMap(Self::get_command_map())), // Q_CMDMAP
-            
-            0x03 => Some(SerprogResponse::ProgrammerName("stm32-serprog")), // Q_PGMNAME
-            
-            0x04 => Some(SerprogResponse::SerialBufferSize(512)), // Q_SERBUF
-            
-            0x05 => Some(SerprogResponse::BusTypes(0x08)), // Q_BUSTYPE (SPI only)
-            
-            0x07 => Some(SerprogResponse::OperationBufferSize(512)), // Q_OPBUF
-            
-            0x08 => Some(SerprogResponse::WriteNMaxLen(256)), // Q_WRNMAXLEN
-            
-            0x0F => Some(SerprogResponse::ReadNMaxLen(256)), // Q_RDNMAXLEN
-            
+            0x00 => Some(SerprogResponse::Ack),
+            0x01 => Some(SerprogResponse::InterfaceVersion),
+            0x02 => Some(SerprogResponse::CommandMap(Self::get_command_map())),
+            0x03 => Some(SerprogResponse::ProgrammerName("stm32-serprog")),
+            0x04 => Some(SerprogResponse::SerialBufferSize(512)),
+            0x05 => Some(SerprogResponse::BusTypes(0x08)),
+            0x07 => Some(SerprogResponse::OperationBufferSize(512)),
+            0x08 => Some(SerprogResponse::WriteNMaxLen(256)),
+            0x0F => Some(SerprogResponse::ReadNMaxLen(256)),
+
             0x0B => {
-                // O_INIT
                 self.spi_initialized = true;
                 Some(SerprogResponse::Ack)
             }
-            
+
             0x0C => {
-                // O_DELAY - expect 4 more bytes
                 self.state = ParseState::ODelay { bytes_remaining: 4 };
                 None
             }
-            
-            0x0E => Some(SerprogResponse::Ack), // SYNCNOP
-            
+
+            0x0E => Some(SerprogResponse::Ack),
+
             0x10 => {
-                // S_SPI_OP - expect 6 more bytes (write_len:3, read_len:3)
                 self.buffer_pos = 0;
-                self.state = ParseState::SSpiOp {
-                    bytes_remaining: 6,
-                    write_len: 0,
-                    read_len: 0,
-                };
+                self.state = ParseState::SSpiOp { bytes_remaining: 6 };
                 None
             }
-            
+
             0x11 => {
-                // S_BUSTYPE - expect 1 more byte
                 self.state = ParseState::SBustype;
                 None
             }
-            
+
             0x12 => {
-                // O_SPIOP
                 if self.spi_initialized {
                     Some(SerprogResponse::Ack)
                 } else {
                     Some(SerprogResponse::Nak)
                 }
             }
-            
-            0x13 => Some(SerprogResponse::Ack), // S_PIN_STATE
-            
-            _ => Some(SerprogResponse::Nak), // Unknown command
+
+            0x13 => Some(SerprogResponse::Ack),
+
+            _ => Some(SerprogResponse::Nak),
         }
     }
-    
-    fn execute_spi_op<SPI, CS, E>(
+
+    fn execute_spi_op<SPI, CS>(
         &mut self,
-        mut write_len: usize,
-        read_len: usize,
         spi: &mut SPI,
         cs: &mut CS,
     ) -> SerprogResponse
     where
-        SPI: Transfer<u8, Error = E>,
+        SPI: SpiBus<u8>,
         CS: OutputPin,
     {
-        // Parse lengths from first 6 bytes
-        write_len = (self.buffer[0] as usize) 
+        let write_len = (self.buffer[0] as usize)
             | ((self.buffer[1] as usize) << 8)
             | ((self.buffer[2] as usize) << 16);
-        
+
         let read_len = (self.buffer[3] as usize)
             | ((self.buffer[4] as usize) << 8)
             | ((self.buffer[5] as usize) << 16);
-        
+
         if write_len > 256 || read_len > 256 {
             return SerprogResponse::Nak;
         }
-        
-        // CS low
+
         let _ = cs.set_low();
-        
-        // Write phase
+
         for i in 0..write_len {
-            let byte = self.buffer[6 + i];
-            let mut data = [byte];
-            let _ = spi.transfer(&mut data);
-        }
-        
-        // Read phase
-        for i in 0..read_len {
-            let mut data = [0xFF];
-            match spi.transfer(&mut data) {
-                Ok(result) => self.buffer[1 + i] = result[0],
-                Err(_) => {
-                    let _ = cs.set_high();
-                    return SerprogResponse::Nak;
-                }
+            let mut data = [self.buffer[6 + i]];
+            if spi.transfer_in_place(&mut data).is_err() {
+                let _ = cs.set_high();
+                return SerprogResponse::Nak;
             }
         }
-        
-        // CS high
+
+        for i in 0..read_len {
+            let mut data = [0xFF];
+            if spi.transfer_in_place(&mut data).is_ok() {
+                self.buffer[1 + i] = data[0];
+            } else {
+                let _ = cs.set_high();
+                return SerprogResponse::Nak;
+            }
+        }
+
         let _ = cs.set_high();
-        
+
         SerprogResponse::ReadData(read_len)
     }
 }
